1fff37b1cc506968eabd99a0530dcd1ca22267e4
Daniel Gatto <d.mario.gatto@gmail.com>
2023-07-11 06:55:11 +0930

    Update KeyboardPageRenderer.cs

--- samples/hardware-keyboard/HardwareKeyboard/HardwareKeyboard.Android/Renderers/KeyboardPageRenderer.cs
+++ samples/hardware-keyboard/HardwareKeyboard/HardwareKeyboard.Android/Renderers/KeyboardPageRenderer.cs
@@ -13,6 +13,7 @@ namespace HardwareKeyboard.Droid.Renderers
     [Preserve(AllMembers = true)]
     public class KeyboardPageRenderer : PageRenderer
     {
+        private bool _disposed;
         private KeyboardPage _page => Element as KeyboardPage;
 
         public KeyboardPageRenderer(Context context) : base(context)
@@ -28,10 +29,15 @@ namespace HardwareKeyboard.Droid.Renderers
             if (Visibility == ViewStates.Visible)
                 RequestFocus();
 
-            _page.Appearing += (sender, args) =>
+            if (e.OldElement != null)
             {
-                RequestFocus();
-            };
+                e.OldElement.Appearing -= PageOnAppearing;
+            } 
+
+            if (e.NewElement != null)
+            {
+                e.NewElement.Appearing += PageOnAppearing;
+            }
         }
 
         public override bool OnKeyUp([GeneratedEnum] Keycode keyCode, KeyEvent e)
@@ -81,6 +87,32 @@ namespace HardwareKeyboard.Droid.Renderers
             }
 
             return handled || base.OnKeyUp(keyCode, e);
+        }
+
+        private void PageOnAppearing(object sender, EventArgs e)
+        {
+            if (_disposed)
+                return;
+
+            RequestFocus();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            _disposed = true;
+
+            if (disposing)
+            {
+                if (Element != null)
+                {
+                    Element.Appearing -= PageOnAppearing;
+                }                
+            }
+
+            base.Dispose(disposing);
         }
     }
 }
